dba55c4a5ae37cf0c21fe4988234a1d6d365f31f
mtiberio <mtiberio>
2000-10-09 18:47:50 +0000

    new files to read http files for java 1/tini

---
+++ micro/src/org/cougaar/microedition/io/FileLoader.java
@@ -0,0 +1,51 @@
+/*
+ * FileLoader.java
+ *
+ * Copyright 2000 by BBN Technologies, LLC. All Rights Reserved
+ *
+ */
+
+package cougaar.microedition.io;
+
+/**
+ * This interface provides a description of a generic file loader.
+ * Versions for the Tini board and KVM will be written.
+ */
+public interface FileLoader {
+
+	/**
+	 * The string where the Protocol, hostName and hostPort are stored.
+	 * Basicly, the URL minus the file and its local path.
+	 * @since   Oct 2, 2000
+	 */
+	String pnp = "";
+
+	/**
+	 * Concatenates the Protocol, hostName and hostPort and stores in pnp.
+	 *
+	 * @param   protocol	"http", etc.
+	 * @param   hostName	"www.bbn.com", etc
+	 * @param   hostPort	use default if = zero, default for http is 80
+	 * @since   Oct 2, 2000
+	 */
+	void configure(String protocol, String hostName, short hostPort);
+
+	/**
+	 * Returns the contents of the pnp String. (pnp=Protocol + hostName + hostPort).
+	 *
+	 * @return  the contents of the String pnp.
+	 * @since   Oct 2, 2000
+	 */
+	String showConfig();
+
+	/**
+	 * opens the URL constructed from the pnp and file name and returns contents.
+	 *
+	 * @param   fileName	file name to be concatenated onto the pnp.
+	 * @return  String that contains the contents of the file
+	 * @exception	possible IO or URL exceptions
+	 * @since   Oct 2, 2000
+	 */
+	String getFile(String fileName) throws Exception;
+
+}
