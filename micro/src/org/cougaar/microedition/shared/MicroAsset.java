6b633bff935367e79a68de1b6548e956243d7451
wwright <wwright>
2000-12-19 19:34:58 +0000

    Make allocations be to assets.  Add cluster lookup capabilities.

---
+++ micro/src/org/cougaar/microedition/shared/MicroAsset.java
@@ -0,0 +1,14 @@
+/*
+ * <copyright>
+ * Copyright 1997-2000 Defense Advanced Research Projects Agency (DARPA)
+ * and ALPINE (A BBN Technologies (BBN) and Raytheon Systems Company
+ * (RSC) Consortium). This software to be used in accordance with the
+ * COUGAAR license agreement.  The license agreement and other
+ * information on the Cognitive Agent Architecture (COUGAAR) Project can
+ * be found at http://www.cougaar.org or email: info@cougaar.org.
+ * </copyright>
+ */
+package cougaar.microedition.shared;
+
+public interface MicroAsset {
+}
