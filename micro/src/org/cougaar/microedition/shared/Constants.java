a7f0e5b614f20a26d95cc6141705d9a271353d19
wwright <wwright>
2001-04-08 22:48:14 +0000

    Add constants for aspect types.  Not complete.

---
+++ micro/src/org/cougaar/microedition/shared/Constants.java
@@ -0,0 +1,12 @@
+package org.cougaar.microedition.shared;
+
+public interface Constants {
+  public interface Aspects {
+    static int LATITUDE = 100;
+    static int LONGITUDE = 101;
+    static int DIRECTION = 102;
+    static int DETECTION = 103;
+    static int IMAGE = 104;
+    static int FLASHLIGHT = 105;
+  }
+}
