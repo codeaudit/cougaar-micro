1b01316e9d0f7929b5f047ebb5c70223ab0536c8
mtiberio <mtiberio>
2001-04-19 18:23:55 +0000

    fix bugs

--- micro/src/org/cougaar/microedition/shared/Constants.java
+++ micro/src/org/cougaar/microedition/shared/Constants.java
@@ -7,10 +7,11 @@ public interface Constants {
     static int LATITUDE = 100;
     static int LONGITUDE = 101;
     static int HEADING = 102;
-    static int DETECTION = 103;
-    static int BEARING = 104;
-    static int IMAGE = 105;
-    static int FLASHLIGHT = 106;
+    static int BEARING = 103;
+    static int DETECTION = 104;
+    static int SCANDIR = 105;
+    static int IMAGE = 106;
+    static int FLASHLIGHT = 107;
 
   }
 
@@ -21,26 +22,24 @@ public interface Constants {
     static String [] roles = { "SurveillanceProvider" };
     static int SURVEILLANCEPROVIDER = 0;
 
-    static String [] meRoles = { "Everything", "PositionProvider", "LocomotionController", "TargetingController", "TurretController", "SONARSensor" };
+    static String [] meRoles = { "Everything", "PositionProvider", "LocomotionController", "TargetingController", "FlashlightController", "TurretController", "SONARSensor" };
     static int EVERYTHING = 0;
     static int POSITIONPROVIDER = 1;
     static int LOCOMOTIONCONTROLLER = 2;
     static int TARGETINGCONTROLLER = 3;
-    static int TURRETCONTROLLER = 4;
-    static int SONARSENSOR = 5;
+    static int FLASHLIGHTCONTROLLER = 4;
+    static int TURRETCONTROLLER = 5;
+    static int SONARSENSOR = 6;
 
-    static String [] verbs = { "ReportPosition", "Advance", "ReportTarget", "TraverseWaypoints", "GetImage", "ControlFlashlight" };
+    static String [] verbs = { "ReportPosition", "Advance", "TraverseWaypoints", "ReportTarget", "ControlFlashlight", "RotateTurret", "ReportDetection", "GetImage" };
     static int REPORTPOSITION = 0;
     static int ADVANCE = 1;
-    static int REPORTTARGET = 2;
-    static int TRAVERSEWAYPOINTS = 3;
-    static int GETIMAGE = 4;
-    static int CONTROLFLASHLIGHT = 5;
-
-    static String [] subVerbs = { "MonitorLocation", "Forward", "AcquireTargetBearing" };
-    static int MONITORLOCATION = 0;
-    static int FORWARD = 1;
-    static int ACQUIRETARGETBEARING = 2;
+    static int TRAVERSEWAYPOINTS = 2;
+    static int REPORTTARGET = 3;
+    static int CONTROLFLASHLIGHT = 4;
+    static int ROTATETURRET = 5;
+    static int REPORTDETECTION = 6;
+    static int GETIMAGE = 7;
 
   }
 
