ac2bb1883176810f1bb046227d46fcd5477d93e4
wwright <wwright>
2001-05-13 15:02:14 +0000

    Add configs for AutonomousAgents conference demo

---
+++ micro/configs/Agents01/Node.bat
@@ -0,0 +1,18 @@
+@echo OFF
+
+REM calls setlibpath.bat which sets the path to the required jar files.
+REM calls setarguments.bat which sets input parameters for system behavior
+CALL %ALP_INSTALL_PATH%\bin\setlibpath.bat
+CALL %ALP_INSTALL_PATH%\bin\setarguments.bat
+
+set MYARGUMENTS= -c -n "%1"
+
+set MYPROPERTIES=-Dorg.cougaar.domain.micro=org.cougaar.microedition.domain.Domain %MYPROPERTIES%
+set LIBPATHS=..\..\data\cougaarMEdomain.jar;..\..\classes;%LIBPATHS%
+
+@ECHO ON
+
+java.exe %MYPROPERTIES% %MYMEMORY% -classpath %LIBPATHS% %MYCLASSES% %MYARGUMENTS% %2 %3
+
+
+
