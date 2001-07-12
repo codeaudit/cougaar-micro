e7697b3523ad82efc751197566cdfbd1816db886
wwright <wwright>
2001-07-12 15:44:04 +0000

    Add utility for killing named TINI processes

---
+++ micro/src/org/cougaar/microedition/tini/KillJava.java
@@ -0,0 +1,32 @@
+package org.cougaar.microedition.tini;
+
+import com.dalsemi.system.*;
+
+/**
+ * Kills a TINI-OS task.
+ * Run: java KillJava.tini <ps substring to kill>
+ * eg: "java KillJava.tini Node.tini"
+ */
+public class KillJava {
+
+  public KillJava(String token) {
+  System.err.println("START");
+    String [] ps = TINIOS.getTaskTable();
+    for (int i=0; i<ps.length; i++) {
+      System.out.println("ps: "+ps[i]);
+      if (ps[i].indexOf(token) != -1) {
+        String pid = ps[i].substring(0, ps[i].indexOf(":"));
+        int ipid = Integer.parseInt(pid);
+        System.out.println("KILL: "+ipid);
+        TINIOS.killTask(ipid);
+      }
+    }
+  }
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.err.println("USAGE: java KillJava.tini <what-to-kill>");
+    } else {
+      KillJava killJava1 = new KillJava("Node.tini");
+    }
+  }
+}
