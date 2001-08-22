a643d83aa58aa169067a9a8906201a25304c0dfb
wwright <wwright>
2001-08-22 20:22:49 +0000

    Update copyright headers

--- micro/src/tutorial/Job.java
+++ micro/src/tutorial/Job.java
@@ -1,10 +1,23 @@
 /*
  * <copyright>
- *  Copyright 1997-2000 Defense Advanced Research Projects
- *  Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
- *  Raytheon Systems Company (RSC) Consortium).
- *  This software to be used only in accordance with the
- *  COUGAAR licence agreement.
+ * 
+ * Copyright 1997-2001 BBNT Solutions, LLC.
+ * under sponsorship of the Defense Advanced Research Projects
+ * Agency (DARPA).
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Cougaar Open Source License as published by
+ * DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ * THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ * PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ * IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ * ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ * HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ * TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THE COUGAAR SOFTWARE.
  * </copyright>
  */
 package tutorial;
@@ -12,7 +25,7 @@ package tutorial;
 /**
  * This class can be published to the PLAN and subscribed to by PlugIns
  * @author ALPINE (alpine-software@bbn.com)
- * @version $Id: Job.java,v 1.1 2000-10-23 14:56:56 mtiberio Exp $
+ * @version $Id: Job.java,v 1.2 2001-08-22 20:22:49 wwright Exp $
  **/
 public class Job {
 
