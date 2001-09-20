ed1b90281cbee3652e6b5365217dbd6159c1c6fa
wwright <wwright>
2001-09-20 21:07:38 +0000

    Add comments for debugging plugins

--- micro/configs/jpl/senode.bat
+++ micro/configs/jpl/senode.bat
@@ -1,12 +1,14 @@
 @echo OFF
 
-CALL %ALP_INSTALL_PATH%\bin\setlibpath.bat
-CALL %ALP_INSTALL_PATH%\bin\setarguments.bat
+CALL %COUGAAR_INSTALL_PATH%\bin\setlibpath.bat
+CALL %COUGAAR_INSTALL_PATH%\bin\setarguments.bat
 
-set MYARGUMENTS= -c -n "%1"
+set LIBPATHS=C:\alp\micro\data\cougaarMEdomain.jar;%LIBPATHS%
 
-set MYPROPERTIES= -Dorg.cougaar.domain.micro=org.cougaar.microedition.se.domain.Domain -Dorg.cougaar.microedition.ServerPort=%2 %MYPROPERTIES% -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false -Dorg.cougaar.core.cluster.enablePublishException=false -Dorg.cougaar.core.cluster.showTraffic=false -Dorg.cougaar.core.cluster.idle.verbose=false
-set LIBPATHS=C:\projects\JPL\micro\data\cougaarMEdomain.jar;%LIBPATHS%
+set MYARGUMENTS=-c -n "%1"
+
+set MYPROPERTIES=-Dorg.cougaar.domain.micro=org.cougaar.microedition.se.domain.Domain -Dorg.cougaar.microedition.ServerPort=1235 %MYPROPERTIES% -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false -Dorg.cougaar.core.cluster.enablePublishException=false -Dorg.cougaar.core.cluster.showTraffic=false -Dorg.cougaar.core.cluster.idle.verbose=false
+
 
 @ECHO ON
 
