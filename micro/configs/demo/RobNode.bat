6e5921a06c9b50e3ecd32c0d122260387452ba21
jmetzger <jmetzger>
2002-01-03 22:09:39 +0000

    Overlooked robot node plugin

--- micro/configs/demo/RobNode.bat
+++ micro/configs/demo/RobNode.bat
@@ -5,10 +5,10 @@ CALL %COUGAAR_INSTALL_PATH%\bin\setarguments.bat
 
 set MYARGUMENTS= -c -n "%1"
 
-set MYPROPERTIES= -Dorg.cougaar.microedition.ServerPort=%2 %MYPROPERTIES% -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false -Dorg.cougaar.core.cluster.enablePublishException=false
-set LIBPATHS=C:\projects\cougaar\micro\TINI\cougaarMEdomain.jar;%LIBPATHS%
+set MYPROPERTIES= -Dorg.cougaar.domain.micro=org.cougaar.microedition.se.domain.Domain -Dorg.cougaar.microedition.ServerPort=%2 %MYPROPERTIES% -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false -Dorg.cougaar.core.cluster.enablePublishException=false
+set LIBPATHS=C:\projects\JXTA\micro\data\cougaarMEdomain.jar;%LIBPATHS%
 
 @ECHO ON
 
-java.exe %MYPROPERTIES% %MYMEMORY% -classpath %LIBPATHS% %MYCLASSES% %MYARGUMENTS%
+C:\JBuilder4\jdk1.3\bin\java.exe %MYPROPERTIES% %MYMEMORY% -classpath %LIBPATHS% %MYCLASSES% %MYARGUMENTS%
 
