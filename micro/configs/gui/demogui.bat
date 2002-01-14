cd40107506c8c1802925db3441c9ee21b21f8927
kjett <kjett>
2002-01-14 20:23:34 +0000

    Add -D argument defining the directory containing the image files used by the software.

--- micro/configs/gui/demogui.bat
+++ micro/configs/gui/demogui.bat
@@ -2,7 +2,8 @@
 
 @set OPENMAPDIR=C:\work\resources\openmap-4.4.2
 @set JXTADIR=C:\jxta
+@set IMGDIR=..\..\data\images
 
-java -cp ..\..\data\cougaarMEdomain.jar;%OPENMAPDIR%\lib\openmap.jar;%OPENMAPDIR%\share\data\shape;%JXTADIR%\shell\lib\jxta.jar;%JXTADIR%\shell\lib\log4j.jar;%JXTADIR%\shell\lib\jxtasecurity.jar;%JXTADIR%\shell\lib\jxtaptls.jar;%JXTADIR%\shell\lib\minimalBC.jar;%JXTADIR%\shell\lib\cryptix32.jar;%JXTADIR%\shell\lib\cryptix-asn1.jar -Dnet.jxta.tls.principal="p2pgui" -Dnet.jxta.tls.password="myp2pgui" org.cougaar.microedition.demo.ugs.P2PDemoUI "file://openmap.properties"
+java -cp ..\..\data\cougaarMEdomain.jar;%OPENMAPDIR%\lib\openmap.jar;%OPENMAPDIR%\share\data\shape;%JXTADIR%\shell\lib\jxta.jar;%JXTADIR%\shell\lib\log4j.jar;%JXTADIR%\shell\lib\jxtasecurity.jar;%JXTADIR%\shell\lib\jxtaptls.jar;%JXTADIR%\shell\lib\minimalBC.jar;%JXTADIR%\shell\lib\cryptix32.jar;%JXTADIR%\shell\lib\cryptix-asn1.jar -Dnet.jxta.tls.principal="p2pgui" -Dnet.jxta.tls.password="myp2pgui" -Dopenmap.iconDir="%IMGDIR%" org.cougaar.microedition.demo.ugs.P2PDemoUI openmap.properties
 
 @endlocal
