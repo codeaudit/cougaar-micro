d45c9217de7b4dabce659059babaa0292af7ab41
wwright <wwright>
2002-11-14 14:16:58 +0000

    Add files to support software reset of SaJe

---
+++ micro/src/org/cougaar/microedition/saje/Restart.java
@@ -0,0 +1,18 @@
+package org.cougaar.microedition.saje;
+import com.ajile.jem.rawJEM;
+/**
+ * This class requires changes to the JVM. See Readme.saje.txt for instructions
+ */
+class Restart {
+// Native menthod for the trap instruction with index 26. 
+public static native void trap26();
+// Native method for halt instruction to restart the JVM. 
+public static native void halt(int code);
+// This method has to be entered in the trap vector table. 
+// which is defined in "Startup.txt". 
+public static void trap26Handler() {
+// The halt instruction must be executed from a trap handler. 
+// The argument of 0 causes a JVM restart. 
+halt(0);
+}
+}
